8f9fa917d10c9afe2a06ca6a152e494b3d726aea
Ray Smets <rayjsmets@gmail.com>
2025-10-21 14:09:35 -0700

    tests

--- tests/services/coordinatorService.test.ts
+++ tests/services/coordinatorService.test.ts
@@ -3,13 +3,40 @@ import { CoordinatorService } from "../../src/services";
 import assert from "node:assert";
 
 describe("CoordinatorService", () => {
-  it("Example test", () => {
+  it("Example happy path test", () => {
     const coordinatorService = new CoordinatorService(
       ["account1", "account2"],
       [{ id: "account1", expiration: 4 }]
     );
 
     assert.strictEqual(coordinatorService.isLocked("account1", 2), true);
+    assert.strictEqual(coordinatorService.isLocked("account1", 4), false);
+    assert.strictEqual(coordinatorService.isLocked("account2", 3), false);
+    assert.strictEqual(coordinatorService.isLocked("account2", 4), false);
+  });
+
+  it("Example invalid account input test", () => {
+    const coordinatorService = new CoordinatorService(
+      ["account1", "account2"],
+      [{ id: "account3", expiration: 4 }]
+    );
+
+    assert.strictEqual(coordinatorService.isLocked("account1", 2), false);
+    assert.strictEqual(coordinatorService.isLocked("account1", 4), false);
+    assert.strictEqual(coordinatorService.isLocked("account2", 3), false);
+    assert.strictEqual(coordinatorService.isLocked("account2", 4), false);
+  });
+
+  it("Example input duplicate locks ids", () => {
+    const coordinatorService = new CoordinatorService(
+      ["account1", "account2"],
+      [
+        { id: "account3", expiration: 4 },
+        { id: "account3", expiration: 0 },
+      ]
+    );
+
+    assert.strictEqual(coordinatorService.isLocked("account1", 2), false);
     assert.strictEqual(coordinatorService.isLocked("account1", 4), false);
     assert.strictEqual(coordinatorService.isLocked("account2", 3), false);
     assert.strictEqual(coordinatorService.isLocked("account2", 4), false);
